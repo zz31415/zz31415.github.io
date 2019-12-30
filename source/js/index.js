1d4ea5e5e67cb31a4ac27c4e0c9b7f0600299c22
ZhangZhao <zz31415@163.com>
2019-12-30 20:19:57 +0800

    upload html

---
+++ source/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $("#event").html('MEET')
+  $("#w").animate({ left: '+=2rem' }, 0);
+  $("#m").animate({ left: '-=2rem' }, 0);
+  $("#event").animate({ opacity: 0 }, 0)
+
+  $("#w").animate({ left: '-=2rem' }, 1500);
+  $("#m").animate({ left: '+=2rem' }, 1500);
+  $("#event").animate({ opacity: 1 }, 1500)
+
+  setTimeout(() => {
+    $("#event").html('KISS')
+  }, 1500)
+  $("#w").animate({ top: '-=2rem' }, 0);
+  $("#m").animate({ top: '+=2rem' }, 0);
+  $("#event").animate({ opacity: 0 }, 0)
+
+  $("#w").animate({ top: '+=2rem' }, 1500);
+  $("#m").animate({ top: '-=2rem' }, 1500);
+  $("#event").animate({ opacity: 1 }, 1500)
+
+  setTimeout(() => {
+    $("#event").html('PROPOSE')
+  }, 3000)
+  $("#w").animate({ left: '+=2rem' }, 0);
+  $("#m").animate({ left: '-=2rem' }, 0);
+  $("#event").animate({ opacity: 0 }, 0)
+
+  $("#w").animate({ left: '-=2rem' }, 1500);
+  $("#m").animate({ left: '+=2rem' }, 1500);
+  $("#event").animate({ opacity: 1 }, 1500)
+
+});
