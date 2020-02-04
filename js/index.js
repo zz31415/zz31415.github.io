558d58f139f2c4f8ee9c04638019ff7524b582cc
ZhangZhao <zz31415@163.com>
2020-02-04 17:32:08 +0800

    upload html

--- js/index.js
+++ js/index.js
@@ -28,6 +28,17 @@ $(document).ready(function () {
 
   $("#w").animate({ left: '-=2rem' }, 1500);
   $("#m").animate({ left: '+=2rem' }, 1500);
-  $("#event").animate({ opacity: 1 }, 1500)
+  $("#event").animate({ opacity: 1 }, 1500);
 
+  propose();
 });
+
+function propose () {
+  $("#propose").hide()
+  setTimeout(() => {
+    $("#event").remove()
+    $("#w").remove()
+    $("#m").remove()
+    $("#propose").show()
+  }, 4500)
+}
